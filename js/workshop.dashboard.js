7a339200b4f3e95dcf62832caa00b82fcb1329a1
Santojeet Chakraborty <santojeetchakraborty@gmail.com>
2026-02-28 02:23:21 +0530

    Fixed workshop login glitch

--- js/workshop.dashboard.js
+++ js/workshop.dashboard.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
     return;
   }
 
-  const username = getUsernameFromToken();
+  const username = localStorage.getItem("workshopUsername") || "User";
   setWorkshopUsername(username);
 
   loadWorkshopStatus();
